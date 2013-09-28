89fe5db823c0b068156486b9aeaeeaa15e2d429a
Paweł Pacana <pawel.pacana@gmail.com>
2013-09-28 15:33:16 +0200

    Improved build console specs.

--- spec/citrus/web/build_console_resource_spec.rb
+++ spec/citrus/web/build_console_resource_spec.rb
@@ -1,13 +1,56 @@
 require 'spec_helper'
 
 describe Citrus::Web::BuildConsoleResource do
+  ENOUGH_TIME_FOR_FIBER_ENCODER_TO_BLOCK = 0.125
 
-  let(:configuration) { fake(:configuration, build_root: '/tmp', cache_root: '/tmp') }
-  let(:app)           { Citrus::Web::Application.new(configuration).webmachine }
+  let(:builds_repository) { fake(:builds_repository) }
+  let(:build)             { fake(:build, output: output) { Citrus::Core::Build } }
+  let(:output)            { fake(:file_output, path: file.path) }
+  let(:file)              { Tempfile.new('file_output') }
 
-  before  { get '/builds/dummy/console' }
+  before { stub(injector).builds_repository { builds_repository } }
 
-  specify { expect(response.code).to eq(200) }
-  specify { expect(response.headers['Content-Type']).to eq('text/event-stream') }
+  context 'GET /builds/:build_id/console' do
+    context 'successfull' do
+      before  { stub(builds_repository).find_by_uuid('1') { build } }
+      subject { get '/builds/1/console' }
+
+      specify { expect(subject.code).to                     eq(200) }
+      specify { expect(subject.headers['Content-Type']).to  eq('text/event-stream') }
+      specify { expect(subject.headers['Connection']).to    eq('keep-alive') }
+      specify { expect(subject.headers['Cache-Control']).to eq('no-cache') }
+
+      it 'should stream existing output data' do
+        file.write('kaka')
+        file.flush
+
+        Webmachine::ChunkedBody.new(subject.body).each do |chunk|
+          expect(chunk).to eq("4\r\nkaka\r\n")
+          break
+        end
+      end
+
+      it 'should stream output data as its written' do
+        Thread.new do
+          sleep(ENOUGH_TIME_FOR_FIBER_ENCODER_TO_BLOCK)
+          file.write('kaka')
+          file.flush
+        end
+
+        Webmachine::ChunkedBody.new(subject.body).each do |chunk|
+          expect(chunk).to eq("4\r\nkaka\r\n")
+          break
+        end
+      end
+    end
+
+    context 'build does not exist' do
+      before  { stub(builds_repository).find_by_uuid('2') { raise Citrus::Web::BuildsRepository::NotFound } }
+      subject { get '/builds/2/console' }
+
+      specify { expect(subject.code).to                     eq(404) }
+      specify { expect(subject.headers['Content-Type']).to  eq('text/html') }
+    end
+  end
 
 end
