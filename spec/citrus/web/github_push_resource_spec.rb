65cbfd1669553c13ad88828c91df97ff3ef37f5f
Paweł Pacana <pawel.pacana@gmail.com>
2013-09-28 13:52:05 +0200

    Improved GithubPushResource specs.

--- spec/citrus/web/github_push_resource_spec.rb
+++ spec/citrus/web/github_push_resource_spec.rb
@@ -2,14 +2,25 @@ require 'spec_helper'
 
 describe Citrus::Web::GithubPushResource do
 
-  let(:configuration) { fake(:configuration, build_root: '/tmp', cache_root: '/tmp') }
-  let(:app)           { Citrus::Web::Application.new(configuration).webmachine }
-  let(:push_data)     { File.read(File.expand_path(File.join(File.dirname(__FILE__), '../../payload.json'))) }
-  let(:payload)       { URI.encode_www_form(payload: push_data) }
+  let(:push_data)      { File.read(File.expand_path(File.join(File.dirname(__FILE__), '../../payload.json'))) }
+  let(:github_payload) { URI.encode_www_form(payload: push_data) }
+  let(:github_adapter) { fake(:github_adapter) { Citrus::Core::GithubAdapter } }
+  let(:changeset)      { fake(:changeset)      { Citrus::Core::Changeset } }
+  let(:create_build)   { fake(:create_build) }
 
-  it 'should accept POST request with github push data in application/json' do
-    post '/github_push', body: payload
-    expect(response.code).to eq(204)
+  before do
+    stub(injector).create_build                                    { create_build }
+    stub(injector).github_adapter                                  { github_adapter }
+    stub(github_adapter).create_changeset_from_push_data(any_args) { changeset }
+  end
+
+  context 'POST /github_push' do
+    before  { post '/github_push', body: github_payload }
+
+    specify { expect(response.code).to  eq(204) }
+    specify { expect(response.body).to  be_nil }
+    specify { expect(github_adapter).to have_received.create_changeset_from_push_data(push_data) }
+    specify { expect(create_build).to   have_received.call(changeset) }
   end
 
 end
