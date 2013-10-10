396f4bff89f667a47b2d6c6c5eb3d20f8ad7d0d6
Paweł Pacana <pawel.pacana@gmail.com>
2013-10-10 22:34:49 +0200

    Make pubsub compatible with non-string messages.

--- lib/citrus/web/pubsub_adapter.rb
+++ lib/citrus/web/pubsub_adapter.rb
@@ -5,33 +5,38 @@ module Citrus
     module NanoPubsubAdapter
       class Publisher
 
-        attr_reader :socket, :address
+        attr_reader :socket, :address, :serializer
 
-        def initialize(address)
-          @address = address
-          @socket  = NanoMsg::PubSocket.new
+        def initialize(address, serializer = Marshal)
+          @address    = address
+          @socket     = NanoMsg::PubSocket.new
+          @serializer = serializer
           socket.bind(address)
         end
 
         def publish(subject, message)
-          socket.send("#{subject} #{message}")
+          socket.send("#{subject} #{serializer.dump(message)}")
         end
 
       end
 
       class Subscriber
 
-        attr_reader :socket, :address
+        attr_reader :socket, :address, :serializer
 
-        def initialize(address)
-          @address = address
-          @socket  = NanoMsg::SubSocket.new
+        def initialize(address, serializer = Marshal)
+          @address    = address
+          @socket     = NanoMsg::SubSocket.new
+          @serializer = serializer
           socket.connect(address)
         end
 
         def subscribe(subject, &block)
           socket.subscribe(subject)
-          loop { block.call(socket.recv.split(' ')[1..-1].join(' ')) }
+          loop do
+            _, message = socket.recv.split(' ', 2)
+            block.call(serializer.load(message))
+          end
         end
 
       end
--- spec/citrus/web/pubsub_adapter_spec.rb
+++ spec/citrus/web/pubsub_adapter_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
+class NullSerializer
+  def dump(object); object; end
+  def load(object); object; end
+end
+
 describe Citrus::Web::NanoPubsubAdapter::Publisher do
 
   let(:address)    { 'inproc://' << SecureRandom.hex }
-  let(:publisher)  { described_class.new(address) }
+  let(:publisher)  { described_class.new(address, serializer) }
   let(:subscriber) { NanoMsg::SubSocket.new.tap { |s| s.connect(address) } }
+  let(:serializer) { NullSerializer.new }
 
   specify { expect(publisher).to respond_to(:publish) }
 
@@ -14,6 +20,15 @@ describe Citrus::Web::NanoPubsubAdapter::Publisher do
     publisher.publish('subject', 'message')
     expect(subscriber.recv).to eq('subject message')
   end
+
+  it 'should serialize data before publishing' do
+    subscriber.subscribe('subject')
+    mock(serializer).dump('message') { 'serialized_message' }
+
+    publisher.publish('subject', 'message')
+    expect(subscriber.recv).to eq('subject serialized_message')
+  end
+
 end
 
 describe Citrus::Web::NanoPubsubAdapter::Subscriber do
@@ -22,8 +37,9 @@ describe Citrus::Web::NanoPubsubAdapter::Subscriber do
   ENOUGH_TIME_TO_EXPECT_NO_PUBLICATION = 2 * ENOUGH_TIME_TO_SUBSCRIBE_BEFORE_PUBLISHING
 
   let(:address)    { 'inproc://' << SecureRandom.hex }
-  let(:subscriber) { described_class.new(address) }
+  let(:subscriber) { described_class.new(address, serializer) }
   let(:publisher)  { NanoMsg::PubSocket.new.tap { |s| s.bind(address) } }
+  let(:serializer) { NullSerializer.new }
 
   specify { expect(subscriber).to respond_to(:subscribe) }
   before  { subscriber } # eager loading socket
@@ -66,6 +82,18 @@ describe Citrus::Web::NanoPubsubAdapter::Subscriber do
     rescue Timeout::Error
     end
     expect(received_data).to eq(nil)
+  end
+
+  it 'should deserialize data when consuming' do
+    received_data = nil
+    Thread.new do
+      sleep(ENOUGH_TIME_TO_SUBSCRIBE_BEFORE_PUBLISHING)
+      publisher.send('subject serialized_message')
+    end
+
+    mock(serializer).load('serialized_message') { 'message' }
+    subscriber.subscribe('subject') { |msg| received_data = msg; break }
+    expect(received_data).to eq('message')
   end
 
 end
