8c5c123305df0e8224d54b6f36c12d628aec1a68
Paweł Pacana <pawel.pacana@gmail.com>
2013-10-28 08:20:54 +0100

    Update mongrel2 adapter.

--- citrus-web.gemspec
+++ citrus-web.gemspec
@@ -25,7 +25,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "rb-inotify",  "~> 0.9.0"
   spec.add_dependency "dependor",    "~> 1.0"
   spec.add_dependency "ffi-rzmq",    "~> 1.0.3"
-  spec.add_dependency "m2r",         "2.1.0.pre"
+  spec.add_dependency "m2r",         "~> 2.1.0.pre"
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
--- lib/m2r_adapter.rb
+++ lib/m2r_adapter.rb
@@ -1,25 +1,16 @@
 require 'm2r'
 require 'm2r/version'
+require 'ostruct'
 require 'uri'
-require 'webmachine/version'
-require 'webmachine/headers'
-require 'webmachine/request'
-require 'webmachine/response'
-require 'webmachine/dispatcher'
 
 module Webmachine
   module Adapters
     class M2R < Adapter
 
       def run
-        adapter_options = {
-          :sender_id => '8699e94e-ee48-4274-9461-5907fa0efc4a',
-          :recv_addr => 'tcp://127.0.0.1:1234',
-          :send_addr => 'tcp://127.0.0.1:4321'
-        }.merge(configuration.adapter_options)
-
-        @handler = Handler.new(@dispatcher, adapter_options)
-        trap('INT', &method(:shutdown))
+        connection = ::M2R::ConnectionFactory.new(OpenStruct.new(adapter_options)).connection
+        @handler = Handler.new(connection, @dispatcher)
+        %w(INT TERM).map { |signal| trap(signal) { shutdown } }
         @handler.listen
       end
 
@@ -27,39 +18,106 @@ module Webmachine
         @handler.stop
       end
 
-      class Handler < ::M2R::Handler
+      protected
 
-        attr_reader :dispatcher
+      def adapter_defaults
+        {
+          :sender_id => '8699e94e-ee48-4274-9461-5907fa0efc4a',
+          :recv_addr => 'tcp://127.0.0.1:1234',
+          :send_addr => 'tcp://127.0.0.1:4321'
+        }
+      end
 
-        def initialize(dispatcher, options)
-          super(::M2R::ConnectionFactory.new(OpenStruct.new(options)), ::M2R::Parser.new)
-          @dispatcher = dispatcher
+      def adapter_options
+        adapter_defaults.merge(configuration.adapter_options)
+      end
+
+      class Handler
+
+        attr_reader :wm_dispatcher, :adapter_options, :request_parser, :poller, :connection
+
+        def initialize(connection, wm_dispatcher, request_parser = ::M2R::Parser.new, poller = ZMQ::Poller.new)
+          @request_parser  = request_parser
+          @wm_dispatcher   = wm_dispatcher
+          @connection      = connection
+          @poller          = poller
         end
 
-        def process(m2r_request)
-          method = m2r_request.method
-          body   = m2r_request.body
-          uri    = request_uri(m2r_request.path, m2r_request.headers)
-
-          headers  = Webmachine::Headers.new(m2r_request.headers.dup)
-          request  = Webmachine::Request.new(method, uri, headers, body)
-          response = Webmachine::Response.new
-          dispatcher.dispatch(request, response)
-
-          response.headers['Server'] = [Webmachine::SERVER_STRING, "M2R/#{::M2R::VERSION}"].join(" ")
-          puts response.inspect
-
-          ::M2R::Reply.new.to(m2r_request)
-                      .status(response.code)
-                      .headers(response.headers)
-                      .body(response.body)
+        def listen
+          poller.register_readable(connection.send(:request_socket))
+          loop do
+            break if stop?
+            poller.poll(100)
+            process(request_parser.parse(connection.receive)) if poller.readables.include?(connection.send(:request_socket))
+          end
+          poller.deregister_readable(connection.send(:request_socket))
+          connection.send(:response_socket).close
+          connection.send(:request_socket).close
         end
 
-        def on_error(request, response, error)
-          raise error
+        def stop
+          @stop = true
         end
 
         protected
+
+        def stop?
+          @stop
+        end
+
+        def process(m2r_request)
+          wm_request   = convert_request(m2r_request)
+          wm_response  = Webmachine::Response.new
+          wm_dispatcher.dispatch(wm_request, wm_response)
+          m2r_response = convert_response(wm_response)
+
+          return stream(m2r_request, m2r_response) if streamable_response(m2r_response)
+          reply(m2r_request, m2r_response)
+        end
+
+        def convert_request(m2r_request)
+          method  = m2r_request.method
+          body    = RequestBody.new(m2r_request)
+          uri     = request_uri(m2r_request.path, m2r_request.headers)
+          headers = Webmachine::Headers[m2r_request.headers.map { |k, v| [k, v] }]
+          Webmachine::Request.new(method, uri, headers, body)
+        end
+
+        def convert_response(wm_response)
+          wm_response.headers['Server'] = "#{Webmachine::SERVER_STRING} M2R/#{::M2R::VERSION}"
+          wm_response.headers.each { |key, value| wm_response.headers[key] = value.join(', ') if value.is_a?(Array) }
+
+          m2r_response = ::M2R::Response.new.status(wm_response.code)
+                                            .headers(wm_response.headers)
+                                            .body(convert_body(wm_response))
+          m2r_response.extend(::M2R::Response::ContentLength) if wm_response.code == 404 # XXX: webmachine bug
+          m2r_response
+        end
+
+        def streamable_response(m2r_response)
+          m2r_response.body.respond_to?(:each)
+        end
+
+        def convert_body(wm_response)
+          body = wm_response.body
+          return ChunkedBody.new(Array(body.call)) if body.respond_to?(:call)
+          return ChunkedBody.new(body)             if body.respond_to?(:each) && wm_response.headers['Transfer-Encoding'] == 'chunked'
+          body
+        end
+
+        def reply(m2r_request, m2r_response)
+          respond(m2r_request, m2r_response)
+        end
+
+        def stream(m2r_request, m2r_response)
+          enumerable_body = m2r_response.body
+          respond(m2r_request, m2r_response.body(nil))
+          enumerable_body.each { |part| respond(m2r_request, part) }
+        end
+
+        def respond(m2r_request, data)
+          connection.deliver(m2r_request.sender, m2r_request.conn_id, data)
+        end
 
         def request_uri(path, headers)
           host_parts = headers['Host'].split(':')
@@ -72,6 +130,21 @@ module Webmachine
           uri_hash[:query] = path_parts.last      if path_parts.length == 2
 
           URI::HTTP.build(uri_hash)
+        end
+      end
+
+      class RequestBody
+
+        def initialize(request)
+          @request = request
+        end
+
+        def to_s
+          @request.body
+        end
+
+        def each
+          yield @request.body
         end
 
       end
